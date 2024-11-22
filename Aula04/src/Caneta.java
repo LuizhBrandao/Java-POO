edb29501137e515a3bf1de6da41fff39b1b2089d
LuizhBrandao <luizh.brandaoo@gmail.com>
2024-11-22 18:57:13 -0300

    Commit aulas 02,03,04 de POO Java

---
+++ Aula04/src/Caneta.java
@@ -0,0 +1,53 @@
+public class Caneta {
+    private String modelo;
+    private float ponta;
+    private String cor;
+    private boolean tampada;
+
+    public Caneta(String modelo, String cor,float ponta) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ponta = ponta;
+        this.tampada = true;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+
+    public void status() {
+        System.out.println("Sobre a Caneta:");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Cor: " + this.getCor());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Tampada: "+ this.tampada +"\n");
+    }
+}
---
+++ Aula04/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("Bic", "Vermelha", 0.7f);
+        c1.status();
+        Caneta c2 = new Caneta("Bic", "Preta", 1.0f);
+        c2.status();
+    }
+}
